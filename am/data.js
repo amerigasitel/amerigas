7acd277a20004f6f3a39a15aae1067d3eefaaa12
amerigasitel <92158147+amerigasitel@users.noreply.github.com>
2021-10-11 01:33:32 -0800

    Add files via upload

---
+++ am/data.js
@@ -0,0 +1,137 @@
+let jap = [
+{
+	elem: "button",
+	appenTo: "#tab-btn",
+	id: "tab-btn-0",
+	class: "tab-btns call-flow",
+	html: "Call Flow"
+},
+{
+	elem: "button",
+	appenTo: "#tab-btn",
+	id: "tab-btn-1",
+	class: "tab-btns call-flows",
+	onclick: "fxnTab(this)",
+	html: "Call Flows"
+},
+{
+	elem: "div",
+	appenTo: "#tabs",
+	id: "tabs-1",
+	class: "tabs-item tab-btn-1",
+	html: "tabs-1"
+},
+{
+	elem: "button",
+	appenTo: "#tab-btn",
+	id: "tab-btn-2",
+	class: "tab-btns call-driver-images",
+	onclick: "fxnTab(this)",
+	html: "Call Driver Images"
+},
+{
+	elem: "div",
+	appenTo: "#tabs",
+	id: "tabs-2",
+	class: "tabs-item tab-btn-2",
+	html: "tabs-2"
+},
+
+]
+
+/*let jap = [
+{
+	recno: 1,
+	id: "tab-btn-1",
+	clas: "tab-btns call-flow",
+	elem: "button",
+	html: "Call Flow"
+},
+	recno: 2,
+	id: "tab-btn-2",
+	clas: "tab-btns call-driver-images",
+	elem: "button",
+	html: "Call Driver Images"
+},	
+{
+	recno: 100,
+	id: "",
+	clas: "",
+	elem: "button",
+	html: "End"
+	title: "",
+	type: "",
+	value: ""
+}
+]*/
+
+/*let japi = {
+	"element": [
+	{
+		"recno": 1,
+		"id":"tab-btn-1",
+		"class": "tab-btns call-flow",
+		"element": "button",
+		"html": "Call Flow"
+	},
+		"recno": 2,
+		"id":"tab-btn-2",
+		"class": "tab-btns call-driver-images",
+		"element": "button",
+		"html": "Call Driver Images"
+	},	
+	{
+		"recno": 100,
+		"id":"",
+		"class": "",
+		"title": "",
+		"type": "",
+		"value": ""
+	}
+	
+	],
+	"kb":[]
+}
+*/
+
+let cdList = [
+{
+	id: 1,
+	issue: "Make Payment",
+	case: "Sold To",
+	caseType: "",
+	caseSubType: "",
+	process: ""
+},
+{
+	id: 1,
+	issue: "Down Payment",
+	case: "Sold To",
+	caseType: "",
+	caseSubType: "",
+	process: ""
+},
+{
+	id: 1,
+	issue: "Reverse / Refund Payment",
+	case: "Sold To",
+	caseType: "Billing A/R",
+	caseSubType: "Refund",
+	process: ""
+},
+
+]
+/*
+	{
+	id: 1,
+	issue: "",
+	case: "",
+	caseType: "",
+	caseSubType: "",
+	notes: "",
+	process: ""
+	},
+
+*/
+const cdTodo = [
+]
